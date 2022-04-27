aa7462ecb9cd92944765df75305e0f9b803592fd
Tub <nilstobiasekman@gmail.com>
2022-04-27 12:07:45 +0200

    item service and repository

---
+++ src/main/java/service/ItemService.java
@@ -0,0 +1,21 @@
+package service;
+
+import entity.Item;
+import org.springframework.stereotype.Service;
+import repository.ItemRepository;
+
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    public ItemService(ItemRepository itemRepository) {
+        this.itemRepository = itemRepository;
+    }
+
+    public Item createItem(Item item) {
+        return itemRepository.save(item);
+    }
+
+
+}
